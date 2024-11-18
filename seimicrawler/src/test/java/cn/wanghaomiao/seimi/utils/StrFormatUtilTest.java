3811e83ce75b34c2177dff71328b25319ef76927
AutoDev <autodev@microsoft.com>
2024-11-18 17:57:28 +0000

    Task 1: Upgrade Java project to Java 21 along with Spring Boot, JUnit.

--- seimicrawler/src/test/java/cn/wanghaomiao/seimi/utils/StrFormatUtilTest.java
+++ seimicrawler/src/test/java/cn/wanghaomiao/seimi/utils/StrFormatUtilTest.java
@@ -1,7 +1,8 @@
 package cn.wanghaomiao.seimi.utils;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * StrFormatUtil Tester.
@@ -9,16 +10,16 @@ import org.junit.Test;
  * @author SeimiMaster seimimaster@gmail.com
  * @version 1.0
  */
-public class StrFormatUtilTest {
+class StrFormatUtilTest {
 
     /**
      * Method: parseCharset(String target)
      */
     @Test
-    public void testParseCharset() throws Exception {
+    void testParseCharset() throws Exception {
         String t = "text/html; charset=utf-16;text/html; charset=utf-8";
         String r = StrFormatUtil.parseCharset(t);
         System.out.println(r);
-        Assert.assertTrue("utf-8".equals(r));
+        assertEquals("utf-8", r);
     }
 }
